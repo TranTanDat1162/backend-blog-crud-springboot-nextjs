a98931bd28834f9ae453a6d6c6ef75261a0b14ed
TranTanDat1162 <dattt11062002@gmail.com>
2024-05-28 14:40:53 +0700

    Update FE Deploy (second time)

--- src/main/java/com/crud/blog/configuration/WebConfig.java
+++ src/main/java/com/crud/blog/configuration/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Do something
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
