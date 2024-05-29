78895081dadf831b34c56fffa44ff9e4d4efb563
TranTanDat1162 <dattt11062002@gmail.com>
2024-05-29 09:31:30 +0700

    Update Origin

--- src/main/java/com/crud/blog/controller/BlogController.java
+++ src/main/java/com/crud/blog/controller/BlogController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/api/blogs")
 public class BlogController {
     private final BlogService blogService;
